acb37d2ae9456632513dd39f96629c68d8e26588
drupalex <drupalex@drupalex-ubuntu>
2015-04-23 01:03:16 +0700

    replace http by express

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var proxyTarget = 'lorempixel.com';
 var serverPort = 8091;
 
-
-var http = require('http'),
+var express = require('express'),
+    querystring = require('querystring'),
     httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createServer({});
 
-http.createServer(function (req, res) {
-  proxy.web(req, res, {
+var app = express();
+
+app.all('/:width/:height', function(req, res) {
+  var proxyUrl = '/' + req.params.width + '/' + req.params.height + '/';
+  req.url = proxyUrl;
+  return proxy.proxyRequest(req, res, {
     target: 'http://' + proxyTarget,
     headers: {
       host: proxyTarget
     }
   });
-}).listen(serverPort);
+});
 
-console.log('http server started on port ' + serverPort + ' ...');
+var server = app.listen(serverPort, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
