d0476140b8c879f9a8c23a0d221051ba2b813608
pnhung177 <pnhung177@gmail.com>
2015-05-03 22:28:51 +0700

    approve the image cache/store folders

--- app/index.js
+++ app/index.js
@@ -1,21 +1,22 @@
+var logger = require('../lib/logger');
+var express = require('express');
+var app = express();
+
 var proxifyList = [
   { host: 'lorempixel.com', port: 80, path: '/%width%/%height%/'}
 ];
-var serverPort = 7700;
-
-var logger = require('../lib/logger');
-var express = require('express');
-
-var app = express();
+var SERVER_PORT = process.env.PORT || 7700;
 
 app.use('/loremphoto', require('../lib/bootstrap.js')({
   urlRoot : 'loremphoto',
-  proxifyList: proxifyList
+  proxifyList: proxifyList,
+  PHOTO_STORE_DIR: process.env.STORE_DIR || '.store',
+  PHOTO_CACHE_DIR: process.env.CACHE_DIR || '.cache'
 }), function(req, res, next) {
   return res.render('loremphoto', { });
 });
 
-var server = app.listen(serverPort, function () {
+var server = app.listen(SERVER_PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
   logger.info('Example app listening at http://%s:%s', host, port);
